32761cf6d3b9b470e9b5afbda528e91097ac19ae
cylong1016 <cyl19941016@sina.com>
2016-10-31 03:45:53 +0800

    修改了输出重复结果的bug

--- exam_2016/Q25/main.cpp
+++ exam_2016/Q25/main.cpp
@@ -1,12 +1,13 @@
+// DATE:	2016-10-30
 // 给定有序排列的N个整数，找出其中3个数相加和为0，输出所有的不重复的3个数，要求输出的结果依然有序。
 // 在单行内，输出的顺序和原来一致，每行之间的顺序和第一个数字在原数列中的顺序一致（如果相同则向后依次比较）
 // INPUT :	5
 //			-2 -1 0 1 2
-// OUTPUT：	-2 0 2
+// OUTPUT:	-2 0 2
 //          -1 0 1(无换行)
 // INPUT :	5
 //			2 2 0 -2 -4
-// OUTPUT：	2 2 -4
+// OUTPUT:	2 2 -4
 //			2 0 -2(无换行)
 
 #include <iostream>
@@ -14,8 +15,9 @@
 
 using namespace std;
 
-void findResultIsZero(vector<int> arr, bool asc) {
+vector<vector<int>> findResultIsZero(vector<int> arr, bool asc) {
 	int size = arr.size();
+	vector<vector<int>> result;
 	for (int i = 0; i < size; i++) {
 		int m = i + 1;
 		int n = size - 1;
@@ -28,13 +30,24 @@ void findResultIsZero(vector<int> arr, bool asc) {
 				asc ? m++ : n--;
 			}
 			else {
-				cout << arr[i] << " " << arr[m] << " " << arr[n] << endl;
-				m++;
-				n--;
+				vector<int> v(3);
+				v[0] = arr[i];
+				v[1] = arr[m];
+				v[2] = arr[n];
+				result.push_back(v);
+				do {
+					m++;
+				} while (m < n && arr[m - 1] == arr[m]);
+				do {
+					n--;
+				} while (m < n && arr[n + 1] == arr[n]);
 			}
 		}
+		while (i < size - 2 && arr[i + 1] == arr[i]) {
+			i++;
+		}
 	}
-
+	return result;
 }
 
 int main() {
@@ -49,7 +62,10 @@ int main() {
 		}
 	}
 
-	findResultIsZero(arr, asc);
+	vector<vector<int>> result = findResultIsZero(arr, asc);
+	for each (vector<int> v in result) {
+		cout << v[0] << " " << v[1] << " " << v[2] << endl;
+	}
 	int a;
 	cin >> a;
 }
